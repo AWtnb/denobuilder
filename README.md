926851b34ddca14a1f585d4ab42697b8893d6cf3
awtnb <arikaebanataw@gmail.com>
2024-10-30 12:59:58 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# README
+
+deno compile and copy exe to directory specified by `.env` .
+
+```PowerShell
+irm https://raw.githubusercontent.com/AWtnb/denobuilder/refs/heads/main/main.ps1 | iex
+```
---
+++ main.ps1
@@ -0,0 +1,42 @@
+@'
+$EXENAME = $args[0]
+if ($EXENAME.length -lt 1) {
+    $EXENAME = (($pwd).Path | Split-Path -Leaf) -replace "^deno-"
+}
+
+deno compile --allow-import --allow-read --allow-write --output $EXENAME .\main.ts
+
+if ($LASTEXITCODE -eq 0) {
+    if (Test-Path .\.env) {
+        $d = (Get-Content .\.env -Raw).Trim()
+        $d = [System.Environment]::ExpandEnvironmentVariables($d)
+        if (-not (Test-Path $d -PathType Container)) {
+            New-Item -Path $d -ItemType Directory
+        }
+        $n = "{0}.exe" -f $EXENAME
+        if (Test-Path $n) {
+            Get-Item $n | Copy-Item -Destination $d -Force
+            "COPIED {0} to: {1}" -f $n, $d | Write-Host -ForegroundColor Blue
+        }
+        else {
+            "{0} not found!" -f $n | Write-Host -ForegroundColor Magenta
+        }
+    }
+    else {
+        ".env not found!" | Write-Host -ForegroundColor Magenta
+    }
+}
+else {
+    "Failed to build. Nothing was copied." | Write-Host -ForegroundColor Magenta
+}
+'@ | Out-File -Path "build.ps1"
+New-Item -Path ".env" -ItemType File -ErrorAction SilentlyContinue
+
+$gi = ".gitignore"
+if (Test-Path $gi) {
+    if (-not (Select-String -Path $gi -Pattern "*.env" -SimpleMatch)) {
+        Write-Output "*.env" | Out-File -FilePath $gi -Append
+    }
+}else {
+    Write-Output "*.env" | Out-File -FilePath $gi
+}
